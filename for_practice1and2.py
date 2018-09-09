18982404ddfe051e8d923e808226ce62b8d2145e
Daria Primorskaya <riokko@rizn.org>
2018-09-09 18:00:29 +0300

    Добавление второго задания

---
+++ for_practice1and2.py
@@ -0,0 +1,14 @@
+# задание по практике 1
+
+plus_one = [34, 21, 54, 2, 46, 32, 57, 38, 92, 5]
+
+for digit in plus_one:
+    digit = digit + 1 
+    print(digit)
+
+# задание по практике 2
+
+text = input("Введите любое предложение, я его выведу вертикально: ")
+
+for letter in text:
+    print(letter)
